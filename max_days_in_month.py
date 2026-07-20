3f2ee015ce4f4047bc21b9dee0eb665cbb06435a
Pranay <pranayvjadhao197@gmail.com>
2026-07-20 10:40:09 +0530

    Initial commit

---
+++ max_days_in_month.py
@@ -0,0 +1,14 @@
+month = int(input("Enter a month number : "))
+
+match month:
+    case 1 | 3 | 5 | 7 | 8 | 10 | 12:
+        print("Max. days are 31")
+
+    case 4 | 6 | 9 | 11:
+        print("Max. days are 30")
+
+    case 2:
+        print("Max. days are 28 or 29")
+
+    case _:
+        print("Invalid month number")
